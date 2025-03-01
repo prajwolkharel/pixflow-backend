c98c24ab493718c1a0b0aee84b1a7a496b113c71
Prajwol Kharel <xyzkharelprajwol@gmail.com>
2025-03-01 22:38:12 +0545

    Change api response

--- src/app.ts
+++ src/app.ts
@@ -2,8 +2,20 @@ import express, { Request, Response } from 'express';
 
 const app = express();
 
+app.use(express.json());
+
+const apiResponse = (
+  res: Response,
+  status: number,
+  success: boolean,
+  message: string,
+  data: any = null
+) => {
+  res.status(status).json({ success, message, data });
+};
+
 app.get('/', (req: Request, res: Response) => {
-  res.send('API is running...');
+  apiResponse(res, 200, true, 'API is running...', null);
 });
 
 export default app;
