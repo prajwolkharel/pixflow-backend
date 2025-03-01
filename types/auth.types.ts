e2f8b43be8f374667c6bd56cf6c7466f1285691b
Prajwol Kharel <xyzkharelprajwol@gmail.com>
2025-03-01 22:50:04 +0545

    Create register request and response type

---
+++ types/auth.types.ts
@@ -0,0 +1,13 @@
+export interface RegisterRequest {
+    name: string;
+    email: string;
+    password: string;
+    role: "ADMIN" | "MANAGER" | "EMPLOYEE";
+}
+
+export interface RegisterResponse {
+    id: string;
+    name: string;
+    email: string;
+    role: string;
+}
